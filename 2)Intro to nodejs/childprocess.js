f90900b146dbc8ff2568da83897c56c8ef93399c
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-05 21:21:19 +0530

    childprocess.js

---
+++ 2)Intro to nodejs/childprocess.js
@@ -0,0 +1,4 @@
+let cp = require("child_process");//the childprocess is a inbuilt module just we ave to store in the variable
+console.log("opening.......");
+cp.execSync("start chrome https://leetcode.com/RANVIR1/");//i will start chrome with help of this command we can start calculator or any file
+console.log("done................");
