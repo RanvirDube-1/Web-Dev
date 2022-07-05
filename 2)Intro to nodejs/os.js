0503d9ca1f34067eb133768774ba01885a5c49c9
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-05 21:24:29 +0530

    os.js

---
+++ 2)Intro to nodejs/os.js
@@ -0,0 +1,7 @@
+let os = require("os");//it helps to make the server of the website
+console.log(os.arch());//prints the information of computer ram or memory
+console.log(os.platform())//give information of te platform of computer
+console.log(os.networkInterfaces());//network or wifi information
+console.log("................");
+//os.cpus() method is an inbuilt application programming interface of the os module which is used to get information about each logical CPU core of the computer.
+console.log(os.cpus());
