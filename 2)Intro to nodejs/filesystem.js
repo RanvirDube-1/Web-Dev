5caeb24fdc88b3b7c1852ee894aa9c0cf78de908
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-17 18:12:09 +0530

    Update filesystem.js more information about it

--- 2)Intro to nodejs/filesystem.js
+++ 2)Intro to nodejs/filesystem.js
@@ -14,3 +14,19 @@ fs.writeFileSync("funds.txt", "$1000000000");//if no file create a file name giv
 
 //to update in the file
        fs.appendFileSync("abx.txt","form space");
+
+//to make directory
+fs.mkdirSync("mydir");
+
+//to make file in my directory
+fs.writeFileSync("mydir/myname.txt");
+
+//inner content of directory
+let content = fs.readdirSync("mydir");//read names of file in my directory
+console.log(content);
+
+for (let i = 0; i < content.length;i++){
+      console.log("file",content[i],"is removed");
+      fs.unlinkSync("mydir/"+content[i]);//to remove files form my directory
+}
+fs.rmdirSync("mydir");//remove my directory
