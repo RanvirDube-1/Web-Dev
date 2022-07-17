fb9b2b9e202b1b1fa02ea5d51b4532a1d85cc1d5
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-17 16:42:25 +0530

    datatest.js

---
+++ 1)Intro-To-JS/datatest.js
@@ -0,0 +1,22 @@
+let zoro = {
+    name: "Roronoa Zoro (ロロノア・ゾロ)",
+    alsoKnowas: "bounty Hunter Zoro (海賊狩りのゾロ)",
+    location: {
+        comeform: "East Blue",
+        town: "Shells Town"
+    }
+    , age: 21,
+    piratesGROUPof: " Straw Hat Pirates (麦わらの一味)",
+    capOFgroup: "Monkey D. Luffy (モンキー・D・ルフィ)",
+    ispirate: true,
+    background: ["bounty Hunter ", "swordmen"],
+    Weight: "85 kg (187 lbs)",
+    Height: "181 cm (5'11)",
+    type: {
+        BloodType: "XF",
+        Species: " Human",
+        Gender:"Male"
+    }
+
+};
+console.log(zoro.name,"also known as ,",zoro.alsoKnowas);
