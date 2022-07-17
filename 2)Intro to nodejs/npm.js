eccc949338ef70121b7b139e53f35bb71c0764ad
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-17 23:21:29 +0530

    npm.js

---
+++ 2)Intro to nodejs/npm.js
@@ -0,0 +1,7 @@
+let calk = require("chalk");
+let figlet=require("figlet");
+console.log(calk.blue("hello world!"));//print blue clour hello world
+console.log(calk.bold("hello world!"));//print bold hello world!
+console.log(calk.underline("hello world!"));//print underline hello world
+console.log(calk.bgBlueBright("hello world!"));//background colour blue
+console.log(calk.red(figlet.textSync("RANVIR1")));
